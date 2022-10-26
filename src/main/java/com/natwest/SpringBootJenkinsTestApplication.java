e6d0f932dae2df43e72701a52f708ea997cb63f4
DineshKumar.Thambiran <dineshkumar.thambiran@natwestmarkets.com>
2022-10-26 15:52:41 +0530

    first commit

---
+++ src/main/java/com/natwest/SpringBootJenkinsTestApplication.java
@@ -0,0 +1,13 @@
+package com.natwest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJenkinsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJenkinsTestApplication.class, args);
+	}
+
+}
